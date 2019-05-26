91098fefddbe17ba8e7688fd891fd50eeeb114d8
Daniel Dimitrov <dimitrov.dan@gmail.com>
2019-05-26 20:53:41 +0200

    Fix var name

--- backup.sh
+++ backup.sh
@@ -3,7 +3,7 @@
 #Configuration variables
 BUCKET_NAME=YOUR_BUCKET_NAME #for example name of the project
 FILE_NAME=YOUR_FILE_NAME #e.g. `date +%Y-%m-%d`.pgsql
-DEST=YOUR_FILE_PATH #e.g. /home/daniel/$FILENAME
+DEST=YOUR_FILE_PATH #e.g. /home/daniel/$FILE_NAME
 PG_USER=USER_NAME #name of the user in PG
 DB_NAME=USER_NAME #name of the DB in PG
 
